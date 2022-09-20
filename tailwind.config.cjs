1a8b899f0d7ba33796312df27a775618a6d9ac85
González Ramón Jesus <jesus.gonzalez@municipio.merida.gob.mx>
2022-09-20 14:55:36 -0500

    Avances en el ejercicio

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,13 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			'monserrat': ['Montserrat', 'serif'],
+			'fraunces': ['Fraunces', 'sans-serif']
+		},
+		colors: {
+			'cyangray': '#3c8067'
+		}
 	},
 	plugins: [],
 }
